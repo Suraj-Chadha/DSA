d8e817a2f61654483b00efe9756949760b5502e0
Suraj-Chadha <s.chadha234@gmail.com>
2022-04-17 23:23:16 +0530

    :art: added balancedBrackets.java

---
+++ DSA Practise/Stacks/balancedBrackets.java
@@ -0,0 +1,51 @@
+// package ;
+
+import java.util.*;
+
+public class balancedBrackets{
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();
+        Stack<Character> st = new Stack<>();
+        for(char ch: str.toCharArray()){
+            if(ch == '(' || ch == '[' || ch == '{'){
+                st.push(ch);
+            }else{
+                if(ch == ')'){
+                    if(!isBalanced(st,'(')){
+                        System.out.println("false");
+                        return;
+                    }
+                    st.pop();
+                }else if(ch == ']'){
+                    if(!isBalanced(st,'[')){
+                        System.out.println("false");
+                        return;
+                    }
+                    st.pop();
+                }else if(ch == '}'){
+                    if(!isBalanced(st,'{')){
+                        System.out.println("false");
+                        return;
+                    }
+                    st.pop();
+                }
+            }
+        }
+        if(st.size() == 0){
+            System.out.println(true);
+        }else{
+            System.out.println(false);
+        }
+        scn.close();
+    }
+    public static boolean isBalanced(Stack<Character> st, char bracket){
+        if(st.size() == 0){
+            return false;
+        }
+        if(st.peek() == bracket){
+            return true;
+        }
+        return false;
+    }
+}
