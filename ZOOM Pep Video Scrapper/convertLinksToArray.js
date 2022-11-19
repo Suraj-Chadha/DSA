72553ecde9533470d23bd1aec03de59ca4a86aba
suraj-chadha <s.chadha234@gmail.com>
2022-11-19 13:26:38 +0530

    Changes Updated

--- ZOOM Pep Video Scrapper/convertLinksToArray.js
+++
@@ -1,8 +0,0 @@
-let fs = require("fs");
-let zoomLinks = fs.readFileSync("zoom.txt","utf-8");
-console.log(zoomLinks);
-let zoomArr = zoomLinks.split('\n');
-// console.log(zoomArr);
-module.export={
-    zoomArr:zoomArr
-}
